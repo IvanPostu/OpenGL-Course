92ab481d557d0dde5806d69f211b73e1fff7e6ba
IvanPostu <ipostu20000127@gmail.com>
2020-08-16 08:38:47 +0300

    Draw simple triangle.

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,53 @@
 #include <iostream>
 #include <cstdlib>
 
+GLfloat point[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f};
+
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f};
+
+const char *vertex_shader = ""
+                            "#version 300\n"
+                            "layout(location = 0) in vec3 vertex_position;"
+                            "layout(location = 1) in vec3 vertex_color;"
+                            "out vec3 color;"
+                            "void main() {"
+                            "   color = vertex_color;"
+                            "   gl_Position = vec4(vertex_position, 1.0);"
+                            "}";
+
+const char *fragment_shader = ""
+                              "#version 300\n"
+                              "in vec3 color;"
+                              "out vec4 frag_color;"
+                              "void main() {"
+                              "   frag_color = vec4(color, 1.0);"
+                              "}";
+
 int g_windowSizeX = 640;
 int g_windowSizeY = 480;
 
-void glfwWindowResizeCallback(GLFWwindow* pWindow, int width, int height)
+void glfwWindowResizeCallback(GLFWwindow *pWindow, int width, int height)
 {
   g_windowSizeX = width;
   g_windowSizeY = height;
   glViewport(0, 0, g_windowSizeX, g_windowSizeY);
 }
 
-void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
+void glfwKeyCallback(GLFWwindow *pWindow, int key, int scancode, int action, int mode)
 {
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+  {
     glfwSetWindowShouldClose(pWindow, GL_TRUE);
   }
-
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   using namespace std;
 
@@ -48,7 +76,8 @@ int main(int argc, char** argv)
 
   glfwMakeContextCurrent(pWindow);
 
-  if(!gladLoadGL()){
+  if (!gladLoadGL())
+  {
     cerr << "Can't load GLAD !" << endl;
     return EXIT_FAILURE;
   }
@@ -58,15 +87,54 @@ int main(int argc, char** argv)
 
   glClearColor(.5f, .2f, 0, 1);
 
-  /* Loop until the user closes the window */
+  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vs, 1, &vertex_shader, nullptr);
+  glCompileShader(vs);
+
+  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fs, 1, &fragment_shader, nullptr);
+  glCompileShader(fs);
+
+  GLuint shader_program = glCreateProgram();
+  glAttachShader(shader_program, vs);
+  glAttachShader(shader_program, fs);
+  glLinkProgram(shader_program);
+
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  GLuint points_vbo = 0;
+  glGenBuffers(1, &points_vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+
+  GLuint colors_vbo = 0;
+  glGenBuffers(1, &colors_vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+  GLuint vao = 0;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+  glEnableVertexAttribArray(1);
+  glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
   while (!glfwWindowShouldClose(pWindow))
   {
-    /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shader_program);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     /* Swap front and back buffers */
     glfwSwapBuffers(pWindow);
-
     /* Poll for and process events */
     glfwPollEvents();
   }
